5dc25668d48caf660d963372bcb6f40b3588d8d5
Andy Osborne <cao89@email.arizona.edu>
2017-10-11 17:02:13 -0700

    Provide script used to create mapped_uris.json

---
+++ RedirectURLS.php
@@ -0,0 +1,56 @@
+<?php
+// Old Press Database
+
+$dsn = "mysql:dbname=" . $db_name_old_press. "press;host=" . $host;
+
+try{
+    $db = new PDO ($dsn, $user_old_press, $pass_old_press);
+} catch (PDOException $e) {
+    echo "Connection falied: " . $e->getMessage();
+    exit();
+}
+
+$query = 'SELECT A.active, A.id, B.isbn FROM book A JOIN isbnformatmap B ON A.id = B.bookid WHERE A.active = "T"';
+
+$ex = $db->prepare($query);
+$ex->execute();
+
+$data_old_press = $ex->fetchAll();
+
+
+//New Press Database
+$dsn = "mysql:dbname=" . $db_name_new_press. "press;host=" . $host;
+
+try{
+    $db = new PDO ($dsn, $user_new_press, $pass_new_press);
+} catch (PDOException $e) {
+    echo "Connection falied: " . $e->getMessage();
+    exit();
+}
+
+$query = 'SELECT A.guid, B.meta_value FROM wp_posts A JOIN wp_postmeta B ON A.ID = post_id WHERE post_type="book" AND meta_key="book_isbn"';
+
+$ex = $db->prepare($query);
+$ex->execute();
+
+$data_new_press = $ex->fetchAll();
+
+$data_new_press_by_isbn = [];
+$mapped_urls = [];
+
+$old_url = "Books/bid";
+
+foreach($data_new_press as $book) {
+    $data_new_press_by_isbn[$book['meta_value']]['url'] = $book['guid']; 
+}
+
+foreach($data_old_press as $old_press_book) {
+    if(isset($data_new_press_by_isbn[$old_press_book['isbn']])) {
+        $mapped_urls[$old_url . $old_press_book['id'] . '.htm']['new_url'] = str_replace($local_wp_site_url,'', $data_new_press_by_isbn[$old_press_book['isbn']]['url']);
+    }
+}
+
+$file = 'mapped_urls.json';
+$handle = fopen($file, 'w');
+fwrite($handle, json_encode($mapped_urls));
+fclose($handle);
---
+++ dbconnect.sample.php
@@ -0,0 +1,14 @@
+<?php
+$host = "127.0.0.1";
+
+//Old Uapress Site Database
+$db_name_old_press = "";
+$user_old_press = "";
+$pass_old_press = "";
+
+//New Uapress Site Database
+$db_name_new_press = "";
+$user_new_press = "";
+$pass_new_press = "";
+
+$local_wp_site_url = "http://localhost/uapress-3/web";
