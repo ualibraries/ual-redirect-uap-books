25c03c7a33db50438d3a00908a654fe0556810b8
Andy Osborne <cao89@email.arizona.edu>
2017-10-13 08:03:19 -0700

    Refactor code and account for multiple isbn's in new uapress database

--- RedirectURLS.php
+++ RedirectURLS.php
@@ -1,6 +1,50 @@
 <?php
 require_once __DIR__ . "/dbconnect.php";
 
+function get_new_data($db) {
+    $query = 'SELECT A.guid, B.meta_value FROM wp_posts A JOIN wp_postmeta B ON A.ID = post_id WHERE post_type="book" AND meta_key="book_formats"';
+
+    $ex = $db->prepare($query);
+    $ex->execute();
+
+    $bookdata = $ex->fetchAll();
+    $reformatted_data = [];
+
+    foreach ($bookdata as $book) {
+        $reformatted_data[] = [
+            "guid" => $book["guid"],
+            "isbns" => get_isbns_from_meta_value($book)
+        ];
+    }
+    return $reformatted_data;
+}
+
+function get_isbns_from_meta_value($book) {
+    $isbns = [];
+    $bookformats = json_decode($book["meta_value"]);
+    foreach($bookformats as $format) {
+        $isbns[] = $format;
+    }
+
+    return $isbns;
+}
+
+function get_old_data($db) {
+    $query = 'SELECT A.active, A.id, B.isbn FROM book A JOIN isbnformatmap B ON A.id = B.bookid WHERE A.active = "T"';
+
+    $ex = $db->prepare($query);
+    $ex->execute();
+
+    $data = $ex->fetchAll();
+    $associated_book_data = [];
+
+    foreach($data as $book_data) {
+        $associated_book_data[$book_data["isbn"]] = $book_data["id"];
+    }
+    return $associated_book_data;
+
+}
+
 // Old UA Press Database
 $dsn = "mysql:dbname=" . $db_name_old_press. ";host=" . $host;
 
@@ -11,12 +55,7 @@ try {
     exit();
 }
 
-$query = 'SELECT A.active, A.id, B.isbn FROM book A JOIN isbnformatmap B ON A.id = B.bookid WHERE A.active = "T"';
-
-$ex = $db->prepare($query);
-$ex->execute();
-
-$data_old_press = $ex->fetchAll();
+$old_data = get_old_data($db);
 
 // New UA Press Database
 $dsn = "mysql:dbname=" . $db_name_new_press. ";host=" . $host;
@@ -28,25 +67,18 @@ try {
     exit();
 }
 
-$query = 'SELECT A.guid, B.meta_value FROM wp_posts A JOIN wp_postmeta B ON A.ID = post_id WHERE post_type="book" AND meta_key="book_isbn"';
+$new_data = get_new_data($db);
 
-$ex = $db->prepare($query);
-$ex->execute();
-
-$data_new_press = $ex->fetchAll();
-
-$data_new_press_by_isbn = [];
 $mapped_urls = [];
 
 $old_url = "Books/bid";
 
-foreach ($data_new_press as $book) {
-    $data_new_press_by_isbn[$book['meta_value']]['url'] = $book['guid'];
-}
-
-foreach ($data_old_press as $old_press_book) {
-    if (isset($data_new_press_by_isbn[$old_press_book['isbn']])) {
-        $mapped_urls[$old_url . $old_press_book['id'] . '.htm']['new_url'] = str_replace($local_wp_site_url, '', $data_new_press_by_isbn[$old_press_book['isbn']]['url']);
+foreach($new_data as $new_book_data) {
+    foreach($new_book_data['isbns'] as $isbn) {
+        if(isset($old_data[$isbn])) {
+            $mapped_urls[$old_url . $old_data[$isbn] . ".htm"] = str_replace($local_wp_site_url, '', $new_book_data['guid']);
+        }
+        break;
     }
 }
 
